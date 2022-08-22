37a6b4dc380901e838ab31c5271f8868916d5cf1
Jan Charris <jancarlosch@outlook.es>
2022-08-22 15:58:53 -0500

    Filtering parameters for the movie entity

---
+++ src/main/java/com/alkemy/management/disney/util/MovieFilter.java
@@ -0,0 +1,29 @@
+package com.alkemy.management.disney.util;
+
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovieFilter {
+  private String title;
+  private Set<Long> genres;
+  private String order;
+
+  public MovieFilter(String title, Set<Long> genres, String order) {
+    this.title = title;
+    this.genres = genres;
+    this.order = order;
+  }
+
+  public boolean isASC() {
+    return this.order.compareToIgnoreCase("ASC") == 0;
+  }
+
+  public boolean isDESC() {
+    return this.order.compareToIgnoreCase("DESC") == 0;
+  }
+
+}
