336c9ac40ec3801b3c19214ec0f2370beb4a2193
Jan Charris <jancarlosch@outlook.es>
2022-08-22 14:33:55 -0500

    Defined a simple DTO for characters

---
+++ src/main/java/com/alkemy/management/disney/dto/SimpleCharacterDTO.java
@@ -0,0 +1,11 @@
+package com.alkemy.management.disney.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SimpleCharacterDTO {
+  private String image;
+  private String name;
+}
