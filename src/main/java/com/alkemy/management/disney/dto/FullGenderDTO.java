dd8ecb94ed01d2d666288859a112f8b7faeaa5f6
Jan Charris <jancarlosch@outlook.es>
2022-08-22 14:40:40 -0500

    A DTO was defined for movie genres

---
+++ src/main/java/com/alkemy/management/disney/dto/FullGenderDTO.java
@@ -0,0 +1,12 @@
+package com.alkemy.management.disney.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FullGenderDTO {
+  private Long id;
+  private String name;
+  private String image;
+}
